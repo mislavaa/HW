b421b14073b2601b36fd089ded8ad71f45d1cf29
mislavaa <mislavaa13@mail.ru>
2024-09-14 20:17:13 +0300

    Homework

---
+++ src/HW9_10.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HW9_10 {
+    public static void main(String[] args) {
+        System.out.println("Проверьте микроволновку");
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        boolean isMicrowaveGood = random.nextBoolean();
+        boolean isCard = random.nextBoolean();
+        System.out.println("Микроволновка подходит? - " + isMicrowaveGood);
+        if (isMicrowaveGood) {
+            System.out.println("Пройдите к кассе");
+            if (isCard) {
+                System.out.print("Способ оплаты: карта" + "\nВведите пин-код: ");
+                int pin = sc.nextInt();
+                System.out.println("Заберите микроволновку");
+            } else {
+                System.out.println("Способ оплаты: наличка" + "\nЗаберите микроволновку");
+            }
+        } else {
+            System.out.println("Выберите другую микроволновку");
+        }
+    }
+}
