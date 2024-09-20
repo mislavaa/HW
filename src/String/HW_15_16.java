c8c1c7cab2e99a538f05f9aa1df93aa108e93a45
mislavaa <mislavaa13@mail.ru>
2024-09-21 01:04:35 +0300

    Homework 15 16

---
+++ src/String/HW_15_16.java
@@ -0,0 +1,31 @@
+package String;
+
+public class HW_15_16 {
+    public static void main(String[] args) {
+        String text = "Java JAVA 12340 567";
+        int lowerLetterCount = 0;
+        int underLetterCount = 0;
+        int numberCount04 = 0;
+        int numberCount59 = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLowerCase(text.charAt(i))) {
+                ++lowerLetterCount;
+            } else if (Character.isUpperCase(text.charAt(i))) {
+                ++underLetterCount;
+            }
+        }
+        System.out.println("Количество прописных букв: " + underLetterCount);
+        System.out.println("Количество строчных букв: " + lowerLetterCount);
+        for (int j = 0; j < text.length(); j++) {
+            if (Character.isDigit(text.charAt(j))) {
+                if (text.charAt(j) <= '4' && text.charAt(j) >= '0') {
+                    ++numberCount04;
+                } else if (text.charAt(j) <= '9' && text.charAt(j) >= '5') {
+                    ++numberCount59;
+                }
+            }
+        }
+        System.out.println("Количество цифр от 0 до 4: " + numberCount04);
+        System.out.println("Количество цифр от 5 до 9: " + numberCount59);
+    }
+}
